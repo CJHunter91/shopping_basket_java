dbfc71d60be363195842c9cdff1db7e6c0cd6e2e
Chris Hunter <orkem2@gmail.com>
2017-08-25 15:55:36 +0100

    added test to check number of items in basket

--- shop_managment_spec/BasketTest.java
+++ shop_managment_spec/BasketTest.java
@@ -3,9 +3,30 @@ import org.junit.*;
 import shop_management.*;
 
 public class BasketTest{
+
+  Basket basket;
+  Item item;
+
+  @Before
+  public void before(){
+    basket = new Basket(false);
+    item = new Item("socks", 20);
+  }
+
   @Test
   public void isLoyal(){
-    Basket basket = new Basket(false);
     assertEquals(false, basket.isLoyal());
   }
+
+  @Test
+  public void canGetEmptyBasketCount(){
+    assertEquals(0, basket.getCount());
+  }
+
+  // @Test
+  // public void canAddItemToBasket(){
+  //   basket.add(item);
+  //   assertEquals(1, )
+  // }
+
 }
