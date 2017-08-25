f26b4ab567555a11440f8498fafd34218b0528bb
Chris Hunter <orkem2@gmail.com>
2017-08-25 15:49:35 +0100

    added test to check can get Price

--- shop_managment_spec/ItemTest.java
+++ shop_managment_spec/ItemTest.java
@@ -5,8 +5,14 @@ import shop_management.*;
 public class ItemTest{
 
   @Test
-  public void getItemName(){
+  public void canGetItemName(){
     Item item = new Item("socks", 10);
     assertEquals("socks", item.getName());
+  }
+
+  @Test
+  public void canGetPrice(){
+    Item item = new Item("socks", 10);
+    assertEquals(10, item.getPrice());
   }
 }
