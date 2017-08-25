7034b4385ef243355da615a004110f9f04087026
Chris Hunter <orkem2@gmail.com>
2017-08-25 15:59:02 +0100

    added getCount to Basket and declared new array in constructor

--- shop_management/Basket.java
+++ shop_management/Basket.java
@@ -1,14 +1,22 @@
 package shop_management;
 
+import java.util.ArrayList;
+
 public class Basket{
 
   private boolean loyalty;
+  private ArrayList<Item> basket;
 
   public Basket(boolean loyalty){
     this.loyalty = loyalty;
+    this.basket = new ArrayList<>();
   }
 
   public boolean isLoyal(){
     return this.loyalty;
+  }
+
+  public int getCount(){
+    return this.basket.size();
   }
 }
