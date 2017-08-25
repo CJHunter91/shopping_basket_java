f210da8fc4771eb29c8461d9e718e32eaf838458
Chris Hunter <orkem2@gmail.com>
2017-08-25 15:47:35 +0100

    added getName to Item class

---
+++ shop_management/Item.java
@@ -0,0 +1,16 @@
+package shop_management;
+
+public class Item{
+
+  private String name;
+  private int price;
+  
+  public Item(String name, int price){
+    this.name = name;
+    this.price = price;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+}
