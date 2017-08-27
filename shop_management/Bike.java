7c9e58d311c88d3be84640fb8d9817c09d80e712
Chris Hunter <orkem2@gmail.com>
2017-08-27 16:21:17 +0100

    added test to check getName

--- shop_management/Bike.java
+++ shop_management/Bike.java
@@ -4,10 +4,17 @@ public class Bike extends Item{
 
   protected int gearNumber;
   protected String colour;
+  protected long id;
 
   public Bike(String colour, int gearNumber, String name, double price){
     super(name, price);
     this.colour = colour;
     this.gearNumber = gearNumber;
+    this.id = 1;
+  }
+
+  @Override
+  public long getId(){
+    return this.id;
   }
 }
--- shop_managment_spec/BikeTest.java
+++ shop_managment_spec/BikeTest.java
@@ -3,10 +3,26 @@ import org.junit.*;
 import shop_management.*;
 
 public class BikeTest{
+  
+  private Bike bike;
+
+  @Before
+  public void before(){
+    bike = new Bike("Red", 20, "Road", 100.0 );
+  }
 
   @Test
   public void canCreateBike(){
-    Bike bike = new Bike("Red", 20, "Road", 100.0 );
     assertEquals(true, bike instanceof Bike);
+  }
+
+  @Test
+  public void canGetBikeId(){
+    assertEquals(1, bike.getId());
+  }
+
+  @Test
+  public void canGetName(){
+    assertEquals("Road", bike.getName());
   }
 }
